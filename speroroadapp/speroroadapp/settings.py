5ccc59d8bcd705d3430468aeb6f45deba7918165
Gonçalo Dias <goncalodias@Goncalos-MacBook-Pro.local>
2014-05-05 17:12:10 +0100

    settings.py fix

--- speroroadapp/speroroadapp/settings.py
+++ speroroadapp/speroroadapp/settings.py
@@ -87,5 +87,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
-STATIC_ROOT =os.path.join(PROJECT_ROOT,'static')
-STATIC_URL = os.path.join(PROJECT_ROOT,'static')
+STATIC_ROOT =os.path.join(BASE_DIR,'roads/static')
+STATIC_URL = os.path.join(BASE_DIR,'roads/static')
