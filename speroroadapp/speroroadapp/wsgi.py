6445a2fdd2be692afcea06eb2cbaad1e4807ca8f
Gonçalo Dias <goncalodias@Goncalos-MacBook-Pro.local>
2014-05-05 18:44:36 +0100

    cenas

--- speroroadapp/speroroadapp/wsgi.py
+++ speroroadapp/speroroadapp/wsgi.py
@@ -10,11 +10,11 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "speroroadapp.settings")
 
-#from django.core.wsgi import get_wsgi_application
+from django.core.wsgi import get_wsgi_application
 
-#application = get_wsgi_application()
+application = get_wsgi_application()
 
  #This application object is used by the development server
 # as well as any WSGI server configured to use this file.
-import django.core.handlers.wsgi
-application = django.core.handlers.wsgi.WSGIHandler()
+#import django.core.handlers.wsgi
+#application = django.core.handlers.wsgi.WSGIHandler()
