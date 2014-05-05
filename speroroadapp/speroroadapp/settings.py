ab298360b95c3bdd9d002f9523a8a9dc99a75866
Gonçalo Dias <goncalodias@Goncalos-MacBook-Pro.local>
2014-05-05 17:26:32 +0100

    settings.py fix

--- speroroadapp/speroroadapp/settings.py
+++ speroroadapp/speroroadapp/settings.py
@@ -11,9 +11,9 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+#BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-#BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 TEMPLATE_DIRS = (
     os.path.join(BASE_DIR, 'roads/templates')
