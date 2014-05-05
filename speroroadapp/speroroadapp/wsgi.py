3d6dfc0434353ca74be7b13dca8e5f36447f9ba1
Gonçalo Dias <goncalodias@Goncalos-MacBook-Pro.local>
2014-05-05 17:42:21 +0100

    changed wsgi.py

--- speroroadapp/speroroadapp/wsgi.py
+++ speroroadapp/speroroadapp/wsgi.py
@@ -11,4 +11,9 @@ import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "speroroadapp.settings")
 
 from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
+#application = get_wsgi_application()
+
+
+from dj_static import Cling
+
+application = Cling(get_wsgi_application())
