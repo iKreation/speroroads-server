374cb64815a6e16d76d38fcf36f3747fd1f650c4
Gonçalo Dias <goncalodias@Goncalos-MacBook-Pro.local>
2014-05-05 18:08:24 +0100

    wsgi

--- speroroadapp/speroroadapp/wsgi.py
+++ speroroadapp/speroroadapp/wsgi.py
@@ -14,7 +14,7 @@ from django.core.wsgi import get_wsgi_application
 
 #application = get_wsgi_application()
 
-
-from dj_static import Cling
-
-application = Cling(get_wsgi_application())
+ #This application object is used by the development server
+# as well as any WSGI server configured to use this file.
+import django.core.handlers.wsgi
+application = django.core.handlers.wsgi.WSGIHandler()
