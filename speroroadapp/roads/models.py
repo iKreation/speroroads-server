c2cc19d39a40d5f76d6fb7fdf976c4880f220d02
coachi <hsanchofilho@gmail.com>
2014-06-04 11:02:01 +0100

    photos

--- speroroadapp/roads/models.py
+++ speroroadapp/roads/models.py
@@ -1,3 +1,18 @@
 from django.db import models
-
+from django.conf import settings
 # Create your models here.
+import os
+from uuid import uuid4
+
+def path_and_rename(path):
+	def wrapper(instance, filename):
+		ext = filename.split('.')[-1]
+		if instance.pk:
+			filename = '{}.{}'.format(instance.pk, ext)
+		else:
+			filename = '{}.{}'.format(uuid4().hex, ext)
+		return os.path.join(path, filename)
+	return wrapper
+
+class TempFile(models.Model):
+    temp = models.FileField(upload_to=path_and_rename("uploads"))
