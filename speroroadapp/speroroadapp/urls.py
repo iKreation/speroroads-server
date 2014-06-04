7cb2594e55a29b3ad80e29e00870ef79b87d3686
coachi <hsanchofilho@gmail.com>
2014-06-04 11:25:22 +0100

    urls.py modification

--- speroroadapp/speroroadapp/urls.py
+++ speroroadapp/speroroadapp/urls.py
@@ -11,5 +11,5 @@ urlpatterns = patterns('roads.views',
 ) + static(settings.STATIC_URL, document_root=settings.STATIC_URL)
 
 urlpatterns += patterns('', 
-	url(r'^media/(?P<path>.*)$', 'django.views.static.serve', { 'document_root': settings.MEDIA_ROOT, })
+	url(r'^media/(?P<path>.*)$', 'django.views.static.serve', { 'document_root': settings.MEDIA_ROOT, 'show_indexes': True, })
 )
